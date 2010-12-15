092534ba9c565af75a280cd71cb812ca6c7c6017
Silinoron <silinoron@gmail.com>
2010-12-14 21:01:09 -0800

    Add code for an unbiased estimator of maximum and minimum damage based on maximum and minimum damage taken.

---
+++ SilinoronParser/Util/DamageStatistics.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SilinoronParser.Util
+{
+    public static class DamageStatistics
+    {
+        public static float MaxDamage(List<float> damageTaken)
+        {
+            float largest = -1;
+            foreach (float f in damageTaken)
+                if (f > largest)
+                    largest = f;
+            return largest + (largest / damageTaken.Count) - 1;
+        }
+
+        public static float MinDamage(List<float> damageTaken)
+        {
+            float smallest = float.MaxValue;
+            foreach (float f in damageTaken)
+                if (f < smallest)
+                    smallest = f;
+            return smallest - (smallest / damageTaken.Count) + 1;
+        }
+    }
+}
